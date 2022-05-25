1cc13f8b71c52203c3d8c3d150417012a4026c8d
Clément E <clement.eraud@lonestone.studio>
2022-05-25 15:38:38 +0200

    Add type helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
-import { ArgFlagCombination, ArgPos, ArgSchema, ArgType } from './schema'
+import { ArgCustomType, ArgFlagCombination, ArgPos, ArgSchema, ArgType } from './schema'
 
 export type ArgOptions<T> = { help?: string; defaultValue?: T }
 
@@ -25,7 +25,7 @@ function _argFlag<T>(type: ArgType<T>, flags: ArgFlagCombination, options?: ArgO
   return _argSchema(type, { type: 'flag', ...flags }, options)
 }
 
-export function positional<T>(type: ArgType<T>, options?: ArgOptions<T>): ArgSchema<T> {
+export function positional<T>(type: Extract<ArgType<T>, { type: 'other' }>, options?: ArgOptions<T>): ArgSchema<T> {
   return _argSchema(type, { type: 'positional' }, options)
 }
 
@@ -44,3 +44,42 @@ export function shortLongFlag<T>(
 ): ArgSchema<T> {
   return _argFlag(type, flag, options)
 }
+
+export const presential: ArgType<boolean> = { type: 'boolean' }
+
+export function arrayOf<T>(type: ArgCustomType<T>): ArgType<T[]> {
+  return { type: 'array', of: type }
+}
+
+export function typed<T>(type: ArgCustomType<T>): ArgType<T> {
+  return { type: 'other', customArgType: type }
+}
+
+export function custom<T>(typename: string, validator: ArgCustomType<T>['validator']): ArgCustomType<T> {
+  return { typename, validator }
+}
+
+export function stringValidator(typename: string, validator: (arg: string) => boolean): ArgCustomType<string> {
+  return custom(typename, (arg) => (validator(arg) ? arg : new Error(`Validation failed`)))
+}
+
+export const string: ArgCustomType<string> = stringValidator('string', (_) => true)
+
+export const integer: ArgCustomType<number> = custom('integer', (arg) => {
+  const num = parseInt(arg)
+
+  if (Number.isNaN(num)) {
+    return new Error('Invalid number provided')
+  }
+
+  if (!Number.isSafeInteger(num)) {
+    return new Error('Please provide a valid integer')
+  }
+
+  return num
+})
+
+export const float: ArgCustomType<number> = custom('float', (arg) => {
+  const num = parseFloat(arg)
+  return Number.isNaN(num) ? new Error('Invalid number provided') : num
+})
